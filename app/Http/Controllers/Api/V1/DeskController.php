820ad09fd54e601224ebac8a5c12f29837628941
Yura <yuravigovskij@gmail.com>
2023-01-24 16:28:41 +0200

    Add resources, Request, DeskController

--- app/Http/Controllers/Api/V1/DeskController.php
+++ app/Http/Controllers/Api/V1/DeskController.php
@@ -3,62 +3,72 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\DeskRequest;
+use App\Http\Resources\DeskResource;
+use App\Models\Desk;
+use App\Models\DeskList;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Symfony\Component\HttpFoundation\Response;
 
 class DeskController extends Controller
 {
+
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return AnonymousResourceCollection
      */
-    public function index()
+    public function index(): AnonymousResourceCollection
     {
-        //
+        return DeskResource::collection(Desk::paginate(5));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param DeskRequest $request
+     * @return DeskResource
      */
-    public function store(Request $request)
+    public function store(DeskRequest $request): DeskResource
     {
-        //
+        return new DeskResource(Desk::create($request->validated()));
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Desk $desk
+     * @return DeskResource
      */
-    public function show($id)
+    public function show(Desk $desk): DeskResource
     {
-        //
+        return new DeskResource($desk);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param DeskRequest $request
+     * @param Desk $desk
+     * @return DeskResource
      */
-    public function update(Request $request, $id)
+    public function update(DeskRequest $request, Desk $desk): DeskResource
     {
-        //
+        $desk->update($request->validated());
+        return new DeskResource($desk);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Desk $desk
+     * @return Response
      */
-    public function destroy($id)
+    public function destroy(Desk $desk): Response
     {
-        //
+        $desk->delete();
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
